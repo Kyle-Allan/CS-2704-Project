d7e1e7bc088eae444a6e00a7753e6b759101971e
Kyle Allan <97403651+Kyle-Allan@users.noreply.github.com>
2023-11-24 18:23:33 -0400

    merging properly now

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ from scipy.stats import pearsonr
 
 
 def mergeDatasets(formal, happy):
-    # merging datasets by country names and dropping unnecessary columns
-    merged = pd.merge(formal, happy, left_on='Entity', right_on='Country', how='inner')
-    return merged
+    # merging datasets by country names
+    mergedDataset = pd.merge(formal, happy, left_on=['Entity', 'Year'], right_on=['Country', 'Year'], how='inner')
+    return mergedDataset
 
 
-def dropUnnecessaryColumns(merged):
-    merged = merged.drop(columns=['Country'])
-    merged = merged.drop(columns=['Code'])
-    merged = merged.drop(columns=['Share of population with no formal education, 1820-2020'])
-    merged = merged.drop(columns=['Year_y'])
-    # removing duplicate country names
-    merged = merged.drop_duplicates(subset=['Entity'])
-    return merged
+def dfSpecificYearOnly(dataframe, year):
+    dataframe = dataframe[dataframe['Year'] == year]
+    return dataframe
 
 
-def printMerged(merged):
-    print(merged)
+def cleanDataframe(df):
+    df = df.drop(columns=['Country'])
+    df = df.drop(columns=['Code'])
+    df = df.drop(columns=['Share of population with no formal education, 1820-2020'])
+    return df
 
 
-def createHeatmap(columns, merged):
-    correlation_matrix = merged[columns].corr()
+def printMerged(mergedDF):
+    print(mergedDF)
+
+
+def createHeatmap(columns, dataFrame):
+    correlation_matrix = dataFrame[columns].corr()
     # creating heatmap of the correlation matrix above
     sb.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
-    mp.title('Correlation Heatmap 2020')
+    mp.title('Correlation Heatmap')
     mp.show()
 
 
@@ -37,33 +39,51 @@ def pearsonCorrelation(educationColumn, happyIndex):
     return pearsonr(educationColumn, happyIndex)
 
 
+def printBasicHappyStatisticsInYear(dataframe, year):
+    dataframe = dataframe[dataframe['Year'] == year]
+    print('Mean happiness index in year', year, ' is ', np.nanmean(dataframe['Index'].tolist()))
 
+
+# datasets
 formalEducation = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\Formal Education.xlsx')
 happinessIndex = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\World Happiness Index by Reports 2013-2023.xlsx')
-
-# dataframe with only values in year 2020
-happy2020 = happinessIndex[happinessIndex['Year'] == 2020]
-formal2020 = formalEducation[formalEducation['Year'] == 2020]
-
 pd.set_option('display.max_columns', None)
 
-# merging datsets
-merged2020 = mergeDatasets(formal2020, happy2020)
+'''
+# dataframe with only values in year 2020
+happy2020 = dfSpecificYearOnly(happinessIndex, 2020)
+formal2020 = dfSpecificYearOnly(formalEducation, 2020)
 
+print(happy2020)
+'''
+
+# merging datasets
+merged = mergeDatasets(formalEducation, happinessIndex)
+merged = cleanDataframe(merged)
+print(merged)
+
+
+merged2020 = dfSpecificYearOnly(merged, 2020)
+print(merged2020)
+
+merged2015 = dfSpecificYearOnly(merged, 2015)
+print(merged2015)
+
+printBasicHappyStatisticsInYear(merged, 2015)
+printBasicHappyStatisticsInYear(merged, 2020)
+
+
+'''
+# creating list of education percent and happiness index where value of year is 2020 so I can do a p value on it
 education2020Column = merged2020['Share of population with some formal education, 1820-2020'].tolist()
 happyindex2020Column = merged2020['Index'].tolist()
 print(pearsonCorrelation(education2020Column, happyindex2020Column))
 
-# dropping unnecessary columns and duplicate entity values
-merged2020 = dropUnnecessaryColumns(merged2020)
-printMerged(merged2020)
+printBasicHappyStatisticsInYear(happinessIndex, 2020)
 
-
-meanHappyIndex2020 = np.nanmean(happyindex2020Column)
-print(meanHappyIndex2020)
-
-
+'''
 # making correlation matrix on the index column and population with formal education
-columns_of_interest = ['Share of population with some formal education, 1820-2020', 'Index']
+columnsOfInterest = ['Share of population with some formal education, 1820-2020', 'Index']
+# heatmap on columns above
+createHeatmap(columnsOfInterest, merged)
 
-createHeatmap(columns_of_interest, merged2020)
