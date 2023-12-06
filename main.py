427c67448895158abd8dbb55e0a5dc2d258a2ab3
Kyle Allan <97403651+Kyle-Allan@users.noreply.github.com>
2023-12-06 10:40:00 -0400

    fixed stuff up but going to test shapiro results

--- main.py
+++ main.py
@@ -92,38 +92,6 @@ def cleanDf(cleanedDataframe):
     return cleanedUpDataFrame
 
 
-pd.set_option('display.max_columns', None)
-
-
-# loading level of education datasets
-enrollmentPrimary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\PrimaryEducationEnrollment.xlsx')
-enrollmentSecondary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\SecondaryEducationEnrollment.xlsx')
-enrollmentTertiary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\TertiaryEducationEnrollment.xlsx')
-# cleaning up dataframes
-enrollmentPrimary = cleanEducationLevelDf(enrollmentPrimary)
-enrollmentSecondary = cleanEducationLevelDf(enrollmentSecondary)
-enrollmentTertiary = cleanEducationLevelDf(enrollmentTertiary)
-# merging level of enrollment of all three types of education into one
-threeLevelOfEducationDf = mergeLevelOfEducationDatasets(enrollmentPrimary, enrollmentSecondary)
-threeLevelOfEducationDf = mergeLevelOfEducationDatasets(threeLevelOfEducationDf, enrollmentTertiary)
-threeLevelOfEducationDf = filterYearsEducationLevelDf(threeLevelOfEducationDf)
-
-
-# loading  population with formal education and happiness index datasets
-formalEducation = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\Formal Education.xlsx')
-happinessIndex = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\World Happiness Index by Reports 2013-2023.xlsx')
-
-# creating merged dataframe of level of education and happiness index
-merged = mergeEducationLevelsWithHappiness(threeLevelOfEducationDf, happinessIndex)
-# filtering dataframe again
-pleaseWork = merged.drop(columns=['Country'])
-pleaseWork = renameEducationLevelDf(pleaseWork)
-# creating dataframe with one entry for each country for year 2015 and 2020
-pleaseWork = createPivotedForLevelOfEducationDf(pleaseWork)
-pleaseWork = pleaseWork.dropna()
-print(pleaseWork)
-
-
 def shapiroTest(data):
     statistic, p_value = shapiro(data)
     print("Test Statistic:", statistic, "P-Value:", p_value)
@@ -151,163 +119,90 @@ def spearmanRankTest(index, education):
     else:
         print('There is not a statistical significant correlation')
 
-print()
-shapiroTest(pleaseWork['2015_Index'])
-shapiroTest(pleaseWork['2020_Index'])
 
-shapiroTest(pleaseWork['2015_secondary enrollment'])
-shapiroTest(pleaseWork['2020_secondary enrollment'])
-shapiroTest(pleaseWork['2015_tertiary enrollment'])
-shapiroTest(pleaseWork['2020_tertiary enrollment'])
-print()
-
-spearmanRankTest(pleaseWork['2015_Index'], pleaseWork['2015_secondary enrollment'])
-spearmanRankTest(pleaseWork['2015_Index'], pleaseWork['2015_tertiary enrollment'])
-print()
-spearmanRankTest(pleaseWork['2020_Index'], pleaseWork['2020_secondary enrollment'])
-spearmanRankTest(pleaseWork['2020_Index'], pleaseWork['2020_tertiary enrollment'])
+pd.set_option('display.max_columns', None)
 
 
-# making scatterplot of tertiary vs secondary enrollment in 2015
-fig, (ax1, ax2) = mp.subplots(1, 2, figsize=(12, 6))
-sb.regplot(x='2015_secondary enrollment', y='2015_Index', data=pleaseWork, color='red', ax=ax1)
-sb.regplot(x='2015_tertiary enrollment', y='2015_Index', data=pleaseWork, color='blue', ax=ax2)
-mp.title('2015')
-mp.show()
-
-# making scatterplot of tertiary vs secondary enrollment in 2020
-fig, (ax1, ax2) = mp.subplots(1, 2, figsize=(12, 6))
-sb.regplot(x='2020_secondary enrollment', y='2020_Index', data=pleaseWork, color='red', ax=ax1)
-sb.regplot(x='2020_tertiary enrollment', y='2020_Index', data=pleaseWork, color='blue', ax=ax2)
-mp.title('2020')
-mp.show()
-
-
-# calculating slope of the line of best fit for tertiary and secondary in 2015
-x_with_constant = sm.add_constant(pleaseWork[['2015_secondary enrollment', '2015_tertiary enrollment']])
-# Fit the linear regression model
-model = sm.OLS(pleaseWork['2015_Index'], x_with_constant).fit()
-# Get the slope (beta1) and (beta2) from the model summary
-slope1 = model.params.iloc[1]
-slope2 = model.params.iloc[2]
-print("Slope (beta1):", slope1, 'Slope (beta2:)', slope2)
-
-
-# calculating slope of the line of best fit for tertiary and secondary in 2020
-x_with_constant = sm.add_constant(pleaseWork[['2020_secondary enrollment', '2020_tertiary enrollment']])
-# Fit the linear regression model
-model = sm.OLS(pleaseWork['2020_Index'], x_with_constant).fit()
-# Get the slope (beta1) and (beta2) from the model summary
-slope1 = model.params.iloc[1]
-slope2 = model.params.iloc[2]
-print("Slope (beta1):", slope1, 'Slope (beta2:)', slope2)
-
-
-# histograms of data for 2015
-fig, axes = mp.subplots(nrows=2, ncols=2, figsize=(12, 10))
-
-# Plot histogram for Happiness Index 2015
-axes[0, 0].hist(pleaseWork['2015_Index'], bins='auto', edgecolor='black', color='skyblue')
-axes[0, 0].set_title('Happiness Index in 2015')
-axes[0, 0].set_xlabel('Happiness Index')
-axes[0, 0].set_ylabel('Frequency')
-# Plot histogram for Happiness Index 2015
-axes[0, 1].hist(pleaseWork['2015_secondary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
-axes[0, 1].set_title('Secondary enrollment in 2015')
-axes[0, 1].set_xlabel('Secondary enrollment')
-axes[0, 1].set_ylabel('Frequency')
-axes[1, 0].axis('off')
-# Plot histogram for Happiness Index 2015
-axes[1, 1].hist(pleaseWork['2015_tertiary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
-axes[1, 1].set_title('Tertiary enrollment in 2015')
-axes[1, 1].set_xlabel('Tertiary enrollment')
-axes[1, 1].set_ylabel('Frequency')
-mp.tight_layout()
-mp.show()
-
-
-# histograms of data for 2020
-fig, axes = mp.subplots(nrows=2, ncols=2, figsize=(12, 10))
-# Plot histogram for Happiness Index 2015
-axes[0, 0].hist(pleaseWork['2020_Index'], bins='auto', edgecolor='black', color='skyblue')
-axes[0, 0].set_title('Happiness Index in 2020')
-axes[0, 0].set_xlabel('Happiness Index')
-axes[0, 0].set_ylabel('Frequency')
-# Plot histogram for Happiness Index 2020
-axes[0, 1].hist(pleaseWork['2020_secondary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
-axes[0, 1].set_title('Secondary enrollment in 2020')
-axes[0, 1].set_xlabel('Secondary enrollment')
-axes[0, 1].set_ylabel('Frequency')
-axes[1, 0].axis('off')
-# Plot histogram for Happiness Index 2020
-axes[1, 1].hist(pleaseWork['2020_tertiary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
-axes[1, 1].set_title('Tertiary enrollment in 2020')
-axes[1, 1].set_xlabel('Tertiary enrollment')
-axes[1, 1].set_ylabel('Frequency')
-mp.tight_layout()
-mp.show()
-
-
-
-'''
-# merging datasets
-merged = mergeDatasets(formalEducation, happinessIndex)
-merged = cleanDataframe(merged)
-
-
+# loading  population with formal education and happiness index datasets
+formalEducation = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\Formal Education.xlsx')
+happinessIndex = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\Education & Happiness\World Happiness Index by Reports 2013-2023.xlsx')
+# merging datasets of formal education and happiness index
+formalEducationHappinessMerged = mergeDatasets(formalEducation, happinessIndex)
+formalEducationHappinessMerged = cleanDataframe(formalEducationHappinessMerged)
+merged = formalEducationHappinessMerged
 # creating dataframe with all relevant info on entity in one row
-cleanedDataframe = createPivotedDf(merged)
-cleanedDataframe = cleanDf(cleanedDataframe)
+formalEducationHappinessMerged = createPivotedDf(formalEducationHappinessMerged)
+formalEducationHappinessMerged = cleanDf(formalEducationHappinessMerged)
 # adding extra calculated data to dataframe
-createYearDifferenceDataColumns(cleanedDataframe)
-
-print(cleanedDataframe)
-
-
-# shapiro test of normality
-statistic_happiness, p_value_happiness = shapiro(cleanedDataframe['2015_Index'])
-statistic_education, p_value_education = shapiro(cleanedDataframe['2015_Share of population with some formal education'])
-print(f"Shapiro-Wilk Test for Happiness Index - Statistic 2015: {statistic_happiness}, P-value: {p_value_happiness}")
-print(f"Shapiro-Wilk Test for Education Levels - Statistic 2015: {statistic_education}, P-value: {p_value_education}")
-
-statistic_happiness, p_value_happiness = shapiro(cleanedDataframe['2020_Index'])
-statistic_education, p_value_education = shapiro(cleanedDataframe['2020_Share of population with some formal education'])
-print(f"Shapiro-Wilk Test for Happiness Index - Statistic 2020: {statistic_happiness}, P-value: {p_value_happiness}")
-print(f"Shapiro-Wilk Test for Education Levels - Statistic 2020: {statistic_education}, P-value: {p_value_education}")
+createYearDifferenceDataColumns(formalEducationHappinessMerged)
+print(formalEducationHappinessMerged)
 
 
-# spearman rank test to test correlation
-corr, pval = spearmanr(cleanedDataframe['2015_Index'], cleanedDataframe['2015_Share of population with some formal education'])
-print("Spearman's rank correlation coefficient for 2015:", corr)
-print("P-value for 2015:", pval)
+# loading level of education datasets
+enrollmentPrimary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\PrimaryEducationEnrollment.xlsx')
+enrollmentSecondary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\SecondaryEducationEnrollment.xlsx')
+enrollmentTertiary = pd.read_excel(r'C:\Users\kylea\OneDrive\2023 Fall Semester\Python\Datasets\TertiaryEducationEnrollment.xlsx')
+# cleaning up dataframes
+enrollmentPrimary = cleanEducationLevelDf(enrollmentPrimary)
+enrollmentSecondary = cleanEducationLevelDf(enrollmentSecondary)
+enrollmentTertiary = cleanEducationLevelDf(enrollmentTertiary)
+# merging level of enrollment of all three types of education into one
+threeLevelOfEducationDf = mergeLevelOfEducationDatasets(enrollmentPrimary, enrollmentSecondary)
+threeLevelOfEducationDf = mergeLevelOfEducationDatasets(threeLevelOfEducationDf, enrollmentTertiary)
+threeLevelOfEducationDf = filterYearsEducationLevelDf(threeLevelOfEducationDf)
 
-corr, pval = spearmanr(cleanedDataframe['2020_Index'], cleanedDataframe['2020_Share of population with some formal education'])
-print("Spearman's rank correlation coefficient for 2020:", corr)
-print("P-value for 2020:", pval)
+# creating merged dataframe of level of education and happiness index
+levelEducationHappinessMerged = mergeEducationLevelsWithHappiness(threeLevelOfEducationDf, happinessIndex)
+# filtering dataframe again
+levelEducationHappinessMerged = levelEducationHappinessMerged.drop(columns=['Country'])
+levelEducationHappinessMerged = renameEducationLevelDf(levelEducationHappinessMerged)
+# creating dataframe with one entry for each country for year 2015 and 2020
+levelEducationHappinessMerged = createPivotedForLevelOfEducationDf(levelEducationHappinessMerged)
+levelEducationHappinessMerged = levelEducationHappinessMerged.dropna()
+print(levelEducationHappinessMerged)
+
+
+
+
+print('Shapiro for 2015 index')
+shapiroTest(levelEducationHappinessMerged['2015_Index'])
+print('Shapiro for 2020 index')
+shapiroTest(levelEducationHappinessMerged['2020_Index'])
+print('Shapiro for 2015 formal education')
+shapiroTest(formalEducationHappinessMerged['2015_Share of population with some formal education'])
+print('Shapiro for 2020 formal education')
+shapiroTest(formalEducationHappinessMerged['2020_Share of population with some formal education'])
+
+print('Spearman rank 2015 index and 2015 formal education')
+spearmanRankTest(formalEducationHappinessMerged['2015_Index'], formalEducationHappinessMerged['2015_Share of population with some formal education'])
+print('Spearman rank 2020 index and 2020 formal education')
+spearmanRankTest(formalEducationHappinessMerged['2020_Index'], formalEducationHappinessMerged['2020_Share of population with some formal education'])
+print()
+
 
 
 fig, axes = mp.subplots(nrows=2, ncols=2, figsize=(12, 10))
 
 # Plot histogram for Happiness Index 2015
-axes[0, 0].hist(cleanedDataframe['2015_Index'], bins='auto', edgecolor='black', color='skyblue')
+axes[0, 0].hist(formalEducationHappinessMerged['2015_Index'], bins='auto', edgecolor='black', color='skyblue')
 axes[0, 0].set_title('Happiness Index in 2015')
 axes[0, 0].set_xlabel('Happiness Index')
 axes[0, 0].set_ylabel('Frequency')
 
 # Plot histogram for Education Levels 2015
-axes[0, 1].hist(cleanedDataframe['2015_Share of population with some formal education'], bins='auto', edgecolor='black', color='lightcoral')
+axes[0, 1].hist(formalEducationHappinessMerged['2015_Share of population with some formal education'], bins='auto', edgecolor='black', color='lightcoral')
 axes[0, 1].set_title('Education Levels in 2015')
 axes[0, 1].set_xlabel('Education Levels')
 axes[0, 1].set_ylabel('Frequency')
 
 # Plot histogram for Happiness Index 2020
-axes[1, 0].hist(cleanedDataframe['2020_Index'], bins='auto', edgecolor='black', color='skyblue')
+axes[1, 0].hist(formalEducationHappinessMerged['2020_Index'], bins='auto', edgecolor='black', color='skyblue')
 axes[1, 0].set_title('Happiness Index in 2020')
 axes[1, 0].set_xlabel('Happiness Index')
 axes[1, 0].set_ylabel('Frequency')
 
 # Plot histogram for Education Levels 2020
-axes[1, 1].hist(cleanedDataframe['2020_Share of population with some formal education'], bins='auto', edgecolor='black', color='lightcoral')
+axes[1, 1].hist(formalEducationHappinessMerged['2020_Share of population with some formal education'], bins='auto', edgecolor='black', color='lightcoral')
 axes[1, 1].set_title('Education Levels in 2020')
 axes[1, 1].set_xlabel('Education Levels')
 axes[1, 1].set_ylabel('Frequency')
@@ -346,7 +241,7 @@ def createAndDisplayBasicHappyStatistics(cleanedDataframe):
     mp.show()
 
 
-createAndDisplayBasicHappyStatistics(cleanedDataframe)
+createAndDisplayBasicHappyStatistics(formalEducationHappinessMerged)
 
 
 def createAndDisplayBasicEducationStatistics(cleanedDataframe):
@@ -381,17 +276,116 @@ def createAndDisplayBasicEducationStatistics(cleanedDataframe):
     mp.show()
 
 
-createAndDisplayBasicEducationStatistics(cleanedDataframe)
-
-
-# making correlation matrix on the index column and population with formal education
-columnsOfInterest = ['Share of population with some formal education, 1820-2020', 'Index']
-# heatmap on columns above
-createHeatmap(columnsOfInterest, merged)
+createAndDisplayBasicEducationStatistics(formalEducationHappinessMerged)
 
 
 sb.lmplot(x='Share of population with some formal education, 1820-2020', y='Index', hue='Year', data=merged, scatter_kws={'s': 50}, height=6, aspect=1.5)
 mp.xlabel('Share of population with some formal education 2015/2020')
 mp.ylabel('Happiness Index')
 mp.show()
+
+
+print('Shapiro for 2015 secondary enrollment')
+shapiroTest(levelEducationHappinessMerged['2015_secondary enrollment'])
+print('Shapiro for 2020 secondary enrollment')
+shapiroTest(levelEducationHappinessMerged['2020_secondary enrollment'])
+print('Shapiro for 2015 tertiary enrollment')
+shapiroTest(levelEducationHappinessMerged['2015_tertiary enrollment'])
+print('Shapiro for 2020 tertiary enrollment')
+shapiroTest(levelEducationHappinessMerged['2020_tertiary enrollment'])
+print()
+
+print('Spearman rank 2015 index and 2015 secondary enrollment')
+spearmanRankTest(levelEducationHappinessMerged['2015_Index'], levelEducationHappinessMerged['2015_secondary enrollment'])
+print('Spearman rank 2015 index and 2015 tertiary enrollment')
+spearmanRankTest(levelEducationHappinessMerged['2015_Index'], levelEducationHappinessMerged['2015_tertiary enrollment'])
+print('Spearman rank 2020 index and 2020 secondary enrollment')
+spearmanRankTest(levelEducationHappinessMerged['2020_Index'], levelEducationHappinessMerged['2020_secondary enrollment'])
+print('Spearman rank 2020 index and 2020 tertiary enrollment')
+spearmanRankTest(levelEducationHappinessMerged['2020_Index'], levelEducationHappinessMerged['2020_tertiary enrollment'])
+
+
+# making scatterplot of tertiary vs secondary enrollment in 2015
+fig, (ax1, ax2) = mp.subplots(1, 2, figsize=(12, 6))
+sb.regplot(x='2015_secondary enrollment', y='2015_Index', data=levelEducationHappinessMerged, color='red', ax=ax1)
+sb.regplot(x='2015_tertiary enrollment', y='2015_Index', data=levelEducationHappinessMerged, color='blue', ax=ax2)
+mp.title('2015')
+mp.show()
+
+# making scatterplot of tertiary vs secondary enrollment in 2020
+fig, (ax1, ax2) = mp.subplots(1, 2, figsize=(12, 6))
+sb.regplot(x='2020_secondary enrollment', y='2020_Index', data=levelEducationHappinessMerged, color='red', ax=ax1)
+sb.regplot(x='2020_tertiary enrollment', y='2020_Index', data=levelEducationHappinessMerged, color='blue', ax=ax2)
+mp.title('2020')
+mp.show()
+
+
+# calculating slope of the line of best fit for tertiary and secondary in 2015
+x_with_constant = sm.add_constant(levelEducationHappinessMerged[['2015_secondary enrollment', '2015_tertiary enrollment']])
+# Fit the linear regression model
+model = sm.OLS(levelEducationHappinessMerged['2015_Index'], x_with_constant).fit()
+# Get the slope (beta1) and (beta2) from the model summary
+slope1 = model.params.iloc[1]
+slope2 = model.params.iloc[2]
+print("Slope (beta1):", slope1, 'Slope (beta2:)', slope2)
+
+
+# calculating slope of the line of best fit for tertiary and secondary in 2020
+x_with_constant = sm.add_constant(levelEducationHappinessMerged[['2020_secondary enrollment', '2020_tertiary enrollment']])
+# Fit the linear regression model
+model = sm.OLS(levelEducationHappinessMerged['2020_Index'], x_with_constant).fit()
+# Get the slope (beta1) and (beta2) from the model summary
+slope1 = model.params.iloc[1]
+slope2 = model.params.iloc[2]
+print("Slope (beta1):", slope1, 'Slope (beta2:)', slope2)
+
+
+# histograms of data for 2015
+fig, axes = mp.subplots(nrows=2, ncols=2, figsize=(12, 10))
+
+# Plot histogram for Happiness Index 2015
+axes[0, 0].hist(levelEducationHappinessMerged['2015_Index'], bins='auto', edgecolor='black', color='skyblue')
+axes[0, 0].set_title('Happiness Index in 2015')
+axes[0, 0].set_xlabel('Happiness Index')
+axes[0, 0].set_ylabel('Frequency')
+# Plot histogram for Happiness Index 2015
+axes[0, 1].hist(levelEducationHappinessMerged['2015_secondary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
+axes[0, 1].set_title('Secondary enrollment in 2015')
+axes[0, 1].set_xlabel('Secondary enrollment')
+axes[0, 1].set_ylabel('Frequency')
+axes[1, 0].axis('off')
+# Plot histogram for Happiness Index 2015
+axes[1, 1].hist(levelEducationHappinessMerged['2015_tertiary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
+axes[1, 1].set_title('Tertiary enrollment in 2015')
+axes[1, 1].set_xlabel('Tertiary enrollment')
+axes[1, 1].set_ylabel('Frequency')
+mp.tight_layout()
+mp.show()
+
+
+# histograms of data for 2020
+fig, axes = mp.subplots(nrows=2, ncols=2, figsize=(12, 10))
+# Plot histogram for Happiness Index 2015
+axes[0, 0].hist(levelEducationHappinessMerged['2020_Index'], bins='auto', edgecolor='black', color='skyblue')
+axes[0, 0].set_title('Happiness Index in 2020')
+axes[0, 0].set_xlabel('Happiness Index')
+axes[0, 0].set_ylabel('Frequency')
+# Plot histogram for Happiness Index 2020
+axes[0, 1].hist(levelEducationHappinessMerged['2020_secondary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
+axes[0, 1].set_title('Secondary enrollment in 2020')
+axes[0, 1].set_xlabel('Secondary enrollment')
+axes[0, 1].set_ylabel('Frequency')
+axes[1, 0].axis('off')
+# Plot histogram for Happiness Index 2020
+axes[1, 1].hist(levelEducationHappinessMerged['2020_tertiary enrollment'], bins='auto', edgecolor='black', color='lightcoral')
+axes[1, 1].set_title('Tertiary enrollment in 2020')
+axes[1, 1].set_xlabel('Tertiary enrollment')
+axes[1, 1].set_ylabel('Frequency')
+mp.tight_layout()
+mp.show()
+
+
+
+'''
+
 '''
